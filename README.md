6db6673f22bb133c5270cdcaf701d7f3e4b7f20c
Luke <luke@justdevtutorials.com>
2023-03-16 11:35:46 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# wx_wrapsizers_tutorial
+
+Using wrap sizers and spacers for UI layout to create a paint app in C++ using wxWidgets.
+
+See: https://www.youtube.com/channel/UC4Q-KGKCeFbBpaAqwllCDqQ
+
+![Paint App](/app.png)
+
+---
+Check out the blog for more! [www.justdevtutorials.com](https://www.justdevtutorials.com)
+---
---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <wx/wx.h>
+
+class MyApp : public wxApp
+{
+public:
+    virtual bool OnInit();
+};
+
+wxIMPLEMENT_APP(MyApp);
+
+class MyFrame : public wxFrame
+{
+public:
+    MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size);
+};
+
+bool MyApp::OnInit()
+{
+    wxInitAllImageHandlers();
+
+    MyFrame *frame = new MyFrame("Hello World", wxDefaultPosition, wxDefaultSize);
+    frame->Show(true);
+    return true;
+}
+
+MyFrame::MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size)
+    : wxFrame(nullptr, wxID_ANY, title, pos, size)
+{
+}
